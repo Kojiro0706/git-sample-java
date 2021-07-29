94381ccfb13796044d3546aa1c3f3f1fcf8343f1
Kojiro0706 <buena_vista0706@i.softbank.jp>
2021-07-29 14:27:49 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleJavaApplication.class, args);
+	}
+
+}
